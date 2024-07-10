c432e2c1b32947decad934bf08e470fe2016c39f
Chico <cdemmenie@gmail.com>
2024-07-10 23:01:26 +0100

    Added url parameter to video_duration and moved 2 functions from videohash.py to videocopy.py, so they can be called from anywhere.

--- videohash2/videoduration.py
+++ videohash2/videoduration.py
@@ -2,18 +2,33 @@ import re
 from shutil import which
 from subprocess import PIPE, Popen
 from typing import Optional
+from .exceptions import DidNotSupplyPathOrUrl
+from .videocopy import (_create_required_dirs_and_check_for_errors,
+                    _copy_video_to_video_dir)
 
 # Module to determine the length of video.
 # The length is found by the FFmpeg, the output of video_duration is in seconds.
 
 
-def video_duration(video_path: str, ffmpeg_path: Optional[str] = None) -> float:
+def video_duration(url: Optional[str] = None,
+                   path: Optional[str] = None,
+                   storage_path: Optional[str] = None,
+                   do_not_copy: Optional[bool] = True,
+                   ffmpeg_path: Optional[str] = None
+                   ) -> float:
+    
     """
     Retrieve the exact video duration as echoed by the FFmpeg and return
     the duration in seconds. Maximum duration supported is 999 hours, above
     which the regex is doomed to fail(no match).
 
-    :param video_path: Absolute path of the video file.
+    :param url: A URL that leads to a video.
+
+    :param path: Absolute path of the video file.
+
+    :param storage_path: Optional, path to where you want to store the video.
+
+    :param do_not_copy: Used when you want to save the video, defaults to True.
 
     :param ffmpeg_path: Path of the FFmpeg software if not in path.
 
@@ -22,10 +37,31 @@ def video_duration(video_path: str, ffmpeg_path: Optional[str] = None) -> float:
     :rtype: float
     """
 
+    if not path and not url:
+        raise DidNotSupplyPathOrUrl(
+            "You must specify either a path or an URL of the video."
+        )
+    
+    if path and url:
+        raise ValueError("Specify either a path or an URL and NOT both.")
+
     if not ffmpeg_path:
         ffmpeg_path = str(which("ffmpeg"))
 
-    command = f'"{ffmpeg_path}" -i "{video_path}"'
+    if url:
+        (video_dir, video_download_dir
+        ) = _create_required_dirs_and_check_for_errors(
+            url=url,
+            storage_path=storage_path
+            )[0:1]
+
+        path = _copy_video_to_video_dir(
+            video_dir,
+            video_download_dir,
+            do_not_copy=do_not_copy
+        )[0]
+
+    command = f'"{ffmpeg_path}" -i "{path}"'
     process = Popen(command, shell=True, stdout=PIPE, stderr=PIPE)
     output, error = process.communicate()
 
